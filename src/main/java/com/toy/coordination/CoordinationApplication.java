b45e5d0026a41baa3d597e1065ea91ed300b116d
ybh89 <akass191@gmail.com>
2023-11-15 16:26:26 +0900

    프로젝트 생성

---
+++ src/main/java/com/toy/coordination/CoordinationApplication.java
@@ -0,0 +1,13 @@
+package com.toy.coordination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinationApplication.class, args);
+	}
+
+}
